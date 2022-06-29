d282e17b81a74a1beba4c8bff11305954204f488
Shashi0225 <90061826+Shashi0225@users.noreply.github.com>
2022-06-28 22:09:38 -0500

    added about.php for about web page

---
+++ OnlinebowlDelivery/about.php
@@ -0,0 +1,17 @@
+<?php
+
+if ($_POST) 
+{
+    echo "<h1></h1>"; 
+}
+
+?>
+<form method="post">
+    <h1 align=center color="blue">USER MANUAL</h1>
+1) User needs to install the xampp server/ mamp for windows/ Mac respectively in their system.<br/>
+2) The user needs to start the xampp server by starting their apache and myql in their xampp control panel.<br/>
+3) The user needs to download the zipfile and extract the zip file and place the zip file in the htdocs inside the xampp<br/> 
+4) The user wants to open PHP Myadmin and then create a database according to the connection file in the project. Then import the sql file to the database from the respective project folder.<br/>
+5) Then the user can run the project by typing localhost/project_folder_name in their web browser.<br/>
+
+</form>
